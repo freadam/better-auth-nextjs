b86fdeea21011c4abc150cea671e56d00995c203
Kinfe123 <kinfishtech@gmail.com>
2025-04-04 06:40:31 +0300

    clean up

--- components/auth/reset-password.tsx
+++ components/auth/reset-password.tsx
@@ -10,7 +10,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 import { useRouter } from "next/navigation";
@@ -19,7 +18,6 @@ import { authClient } from "@/lib/auth-client";
 import { Alert, AlertDescription, AlertTitle, AlertCircle } from "../ui/alert";
 import { Terminal } from "lucide-react";
 import { toast } from "sonner";
-import { IconLoader } from "@tabler/icons-react";
 
 export function ResetPassword({
   className,
