237c27b961288c07d342da9fa66be4776e4daf64
Kinfe123 <kinfishtech@gmail.com>
2025-04-06 01:33:35 +0300

    build and prod keys

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
-
 export async function middleware(request: NextRequest) {
   const sessionCookie = getSessionCookie(request);
   const { pathname } = request.nextUrl;
-
   if (sessionCookie && ["/login", "/signup"].includes(pathname)) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
